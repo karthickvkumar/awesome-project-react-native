2bf983f41d5690b98ef873272234300f74d573c4
kvelraj_concord <kvelraj@concord.net>
2020-12-29 20:34:31 +0530

    first commit

--- App.js
+++ App.js
@@ -4,18 +4,27 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.header}>
+      <Text style={styles.message}>Welcome to React Native</Text>
+      <Text style={styles.information}>Hello Karthick!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  header : {
+    marginTop : 200,
+    marginLeft : 25
   },
-});
+  message : {
+    fontSize : 28,
+    color: 'red',
+    fontWeight: 'bold'
+  },
+  information: {
+    fontSize: 24,
+    color: '#350f96'
+  }
+}) 
+
